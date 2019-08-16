8615dbc34e72b82496c9ffe20b815cfe7127e0f9
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-16 12:32:33 -0700

    recommitting

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -3,20 +3,20 @@ import { Card, Grid } from "semantic-ui-react"
 
 const CharacterCard = props => (
 
-    <Grid.Column padded key={props.id}>
+    <Grid.Column>
         <Card className='cards'>
             <Card.Content>
                 <Card.Header>
                     {props.name}
                 </Card.Header>
                 <Card.Meta>
-                    Height: {props.height}
+                    Height: {props.height}cm.
                 </Card.Meta>
                 <Card.Meta>
                     Hair: {props.hair}
                 </Card.Meta>
                 <Card.Meta>
-                    Mass: {props.mass}
+                    Mass: {props.mass}kg.
                 </Card.Meta>
                 <Card.Description>
                     More Details: <a href={props.link}>{props.link}</a>
